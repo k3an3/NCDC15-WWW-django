3b43444f993ef82b8b7457496ce52a753f87850b
Keane O'Kelley <korockinout13@gmail.com>
2015-01-22 12:00:25 -0600

    Match original URLs more closely

--- NCDC15_WWW_django/urls.py
+++ NCDC15_WWW_django/urls.py
@@ -3,11 +3,7 @@ from django.contrib import admin
 from django.http import HttpResponseRedirect
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'NCDC15_WWW_django.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-    url(r'^$', lambda r: HttpResponseRedirect('cgi-bin/')),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^cgi-bin/', include('cgi_bin.urls', namespace="cgi-bin")),
+    url(r'^', include('cgi_bin.urls', namespace="cgi-bin")),
 )
 
